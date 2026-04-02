7de53c232d522fd2ebb34c83e7d4a3127047876e
Satya Prakash <satya.prakash0715@gmail.com>
2026-04-02 12:23:20 +0530

    updated base url

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:7777";
+  process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const ENDPOINTS = {
   LOGIN: "/login",
